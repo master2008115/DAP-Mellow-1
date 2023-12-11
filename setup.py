0b617acaefc9e6117e00cc1c9a2277d57e438f7f
master2008115 <153284456+master2008115@users.noreply.github.com>
2023-12-11 14:07:59 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='motivational AI',
+    version='1.1',
+    description='A chatbot for motivational qoutes.',
+    author='master',
+    author_email='aniketmalik2009@gmail.com',
+    license='MIT',
+    keywords = ['motivation', 'quotes'],
+    packages=find_packages(),
+    zip_safe=False
+    )
